02889c53c3ab75a930c6f93cd53b85962c078e80
Farouq <harbyodunojo@gmail.com>
2026-06-27 10:47:35 +0100

    Made some adjustment on the time  format and added an alternative to the search submission

--- PlusHomework/index.js
+++ PlusHomework/index.js
@@ -12,6 +12,15 @@ let currentDay = days[now.getDay()];
 let currentHour = now.getHours();
 let currentMinute = now.getMinutes();
 let currentInfo = document.querySelector(".info");
+
+if (currentMinute < 10) {
+	currentMinute = `0${currentMinute}`;
+}
+
+if (currentHour < 10) {
+	currentHour = `0${currentHour}`;
+}
+
 currentInfo.innerHTML = `${currentDay} ${currentHour}:${currentMinute}`;
 
 let searchButton = document.querySelector(".search-button");
@@ -21,3 +30,13 @@ searchButton.addEventListener("click", function (event) {
 	let city = document.querySelector(".current-city");
 	city.innerHTML = `${searchInput.value}`;
 });
+// Alternatively
+// let searchForm = document.querySelector("#search-form");
+
+// function searchSubmitted(event) {
+//   event.preventDefault();
+//   let searchInput = document.querySelector(".search-input");
+//   let city = document.querySelector(".current-city");
+//   city.innerHTML = searchInput.value;
+// }
+// searchForm.addEventListener("submit", searchSubmitted);
